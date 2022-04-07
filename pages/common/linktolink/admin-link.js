f49f0294ee2ba9f9faeea3ca6e82853c2c0bf7af
ChoHyunGook <whgusrnr7755@gmail.com>
2022-04-07 17:30:07 +0900

    2022-04-07 17:29

--- pages/common/linktolink/admin-link.js
+++ pages/common/linktolink/admin-link.js
@@ -7,6 +7,7 @@ export default function MyBasic(){
         <ul>
         <li className={styles.li}> <Link href='/user/sign-in'>SignIn</Link> </li><br/><br/>
         <li className={styles.li}> <Link href='/user/sign-up'>SignUp</Link> </li><br/><br/><br/>
+        <li className={styles.li}> <Link href='/user/user-list'>UserList</Link> </li><br/><br/><br/>
         <button onClick={ () => {history.back('/'); } } >뒤로가기</button>
         </ul>
         </nav>
